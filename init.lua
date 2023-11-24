2c9c02c60972db1f5c8bc76a10df91dd3ef6f6bd
yousl902 <younes.sleibi2000@gmail.com>
2023-11-24 20:40:24 +0100

    formatting

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -18,45 +15,12 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-
-  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
-
-  -- Fuzzy Finder Algorithm which requires local dependencies to be builtg
-  -- Only load if `make` is available. Make sure you have the system
-  -- requirements installed.
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    -- NOTE: If you are having trouble with this installation,
-    --       refer to the README for telescope-fzf-native for more instructions.
-    build = 'make',
-    cond = function()
-      return vim.fn.executable 'make' == 1
-    end,
-  },
-
-     {'mfussenegger/nvim-jdtls'},
-     { "christoomey/vim-tmux-navigator" },
-
--- essential plugins
-     { 'tpope/vim-surround' }, -- add, delete, change surroundings (it's awesome)
-     {'inkarkat/vim-ReplaceWithRegister'}, -- replace with register contents using motion (gr + motion)
-     --{'frazrepo/vim-rainbow'}, -- colored parentheses
-     --{'luochen1990/rainbow'}, -- colored parentheses
-     {'p00f/nvim-ts-rainbow'},
-
-    -- to learn basic vim commands
-     {'ThePrimeagen/vim-be-good'},
-     {"rhysd/vim-clang-format"},
-
-  { import = 'custom.plugins' },
+  { import = 'user.plugins' },
 }, {})
 
 -- [[configurations]]
-require "config.set"
-require "config.keymaps"
---require "config.d,iagnostic"
-require "config.filetree"
-require "config.color"
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+require 'user.mappings'
+require 'user.opts'
+require 'user.autocmds'
+require 'user.configs.colors'
+require 'user.configs.diagnostics'
