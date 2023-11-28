9e2383bf7e538414dfeb0b97defb8631ccd3d2b2
yousl902 <younes.sleibi2000@gmail.com>
2023-11-28 20:01:11 +0100

    tried to change the LineNr hl

--- lua/user/configs/colors.lua
+++ lua/user/configs/colors.lua
@@ -1,7 +1,13 @@
-vim.cmd([[colorscheme lunar]])
+-- vim.cmd([[colorscheme lunar]])
 -- vim.cmd([[colorscheme tokyonight-night]])
--- vim.cmd([[colorscheme kanagawa]])
--- vim.cmd([[colorscheme catppuccin]])
+
+vim.cmd([[colorscheme kanagawa-wave]])
+-- TODO: fix this so that the LineNr dose not have the same color as the background
+-- vim.cmd([[highlight LineNr term=bold cterm=NONE ctermfg=DarkGrey ctermbg=NONE gui=NONE guifg=DarkGrey guibg=#2A2A37]])
+-- vim.cmd([[highlight SignColmn term=bold cterm=NONE ctermfg=DarkGrey ctermbg=NONE gui=NONE guifg=DarkGrey guibg=#2A2A37]])
+-- sumiInk6 = "#54546D",
+
+-- vim.cmd([[colorscheme catppuccin-mocha]])
 -- vim.cmd([[colorscheme onenord]])
 
 -- vim.cmd([[colorscheme onedark]])
